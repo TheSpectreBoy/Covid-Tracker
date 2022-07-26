b6716de2cc31233e6deab2df87abd28041f52408
TheSpectreBoy <78897685+TheSpectreBoy@users.noreply.github.com>
2022-07-26 11:12:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import bs4
 import tkinter as tk
 
 
-
+#defining get_html_data function to collect data from url.
 def get_html_data(url):
     data = requests.get(url)
     return data
 
-
+#defining get_covid_data function to get covid data from given url.
 def get_covid_data():
     url = "https://www.worldometers.info/coronavirus/"
     html_data = get_html_data(url)
@@ -16,7 +16,7 @@ def get_covid_data():
     info_div = bs.find("div", class_="content-inner").findAll("div", id="maincounter-wrap")
     all_data = ""
 
-
+#using 'for' loop for getting all 3 data "CoronaVirus Cases:", "Deaths:", "Recovered:".
     for block in info_div:
         text = block.find("h1", class_=None).get_text()
 
@@ -25,7 +25,7 @@ def get_covid_data():
         all_data = all_data + text + " " + count + "\n"
     return all_data
 
-
+#defining Function get_country_data() to get data for specific country.
 def get_country_data():
     name = textfield.get()
     url = "https://www.worldometers.info/coronavirus/country/"+name
@@ -48,7 +48,7 @@ def get_country_data():
 
 
 
-
+#defining reload function to reload/refresh data
 def reload():
     new_data = get_covid_data()
     mainlabel['text']=new_data
@@ -56,17 +56,18 @@ def reload():
 
 get_covid_data()
 
-
+#using tkinter for UI
 root = tk.Tk()
 root.geometry("900x700")
 root.title("Covid Tracker")
 f = ("poppins", 25, "bold")
 
+#importing "covid.png" in UI
 banner = tk.PhotoImage(file="covid.png")
 bannerlabel = tk.Label(root, image=banner)
 bannerlabel.pack()
 
-
+#creating text field for searching country data
 textfield = tk.Entry(root, width = 50)
 textfield.pack()
 
@@ -75,11 +76,13 @@ mainlabel = tk.Label(root, text=get_covid_data(), font=f)
 mainlabel.pack()
 
 
-
+#creating a button to get country data from function get_country_data()
 gbtn = tk.Button(root, text="Get Data", font=f, relief='solid', command=get_country_data)
 gbtn.pack()
 
+#creating a button to reload from reload function
 rbtn = tk.Button(root, text="Reload", font=f, relief='solid', command=reload)
 rbtn.pack()
 
 root.mainloop()
+
